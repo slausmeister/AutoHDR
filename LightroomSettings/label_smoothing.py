23b0475a618efd919f0602ebd9bf15b8f35581ff
LucasJoh <lucasjohannesson@web.de>
2024-07-04 09:36:54 +0200

    chore: label smoothing

--- LightroomSettings/label_smoothing.py
+++ LightroomSettings/label_smoothing.py
@@ -4,6 +4,10 @@ import torch
 
 
 def smoothing(labels, method='moving_average', window_size=5, sigma=2):
+    """
+    Takes a list of labels and gives back a list of smoothed labeles depending on the chosen method.
+    window_size is parameter for moving average. sigma is a paramter for gaussian smoothing
+    """
     if method == 'moving_average':
         # Apply moving average smoothing
         if window_size > 0:
@@ -19,6 +23,9 @@ def smoothing(labels, method='moving_average', window_size=5, sigma=2):
 
 
 def smooth_labels(label_dict, config={"method":"moving_average", "window_size":3, "sigma":2}):
+    """
+    Takes a dict of labels as produced by DataSet and gives back the corresponding dict with smoothed label values.
+    """
     values_array = torch.stack(list(label_dict.values())).numpy()
     smoothed_labels = np.zeros_like(values_array)
 
@@ -30,30 +37,6 @@ def smooth_labels(label_dict, config={"method":"moving_average", "window_size":3
     smoothed_label_dict = smoothed_label_dict = {key: torch.tensor(smoothed_labels[i]) for i, key in enumerate(label_dict.keys())}
 
     return smoothed_label_dict
-
-
-if __name__ == "__main__":
-
-    # Example labels
-    labels = torch.load("data/label_tensors.pt")
-    # Convert the dictionary values to a 2D numpy array
-    #values_array = np.array(list(labels.values()))
-
-    # Extract the first column
-    #label = np.array([0.2, 0.4, 0.6, 0.8, 1.0, 0.9, 0.7, 0.5, 0.3, 0.1])
-    label = np.array([0.23,0.,0.264,0.244])
-    smooth = smoothing(label, method = "moving_average", window_size=2, sigma=0.5)
-
-    print("Original Labels:", label)
-    print("Smoothed Labels:", smooth)
-
-    # Smooth the labels using moving average
-    original_labels = smooth_labels(labels, {"method":"moving_average", "window_size":0, "sigma":0})
-    smoothed_labels = smooth_labels(labels, {"method":"gaussian", "window_size":2, "sigma":0.5})
-    
-
-    #print("Original Labels:", original_labels)
-    #print("Smoothed Labels:", smoothed_labels)
 
     
     
